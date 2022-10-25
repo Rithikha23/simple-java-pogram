5e605c18a99a770204c1481aae361c064ca78658
Rithikha23 <115273230+Rithikha23@users.noreply.github.com>
2022-10-25 19:07:58 +0530

    Add files via upload

---
+++ SimpleJavaProgram.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple.java.program;
+
+/**
+ *
+ * @author pc
+ */
+public class SimpleJavaProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        {
+            int a[] = {1,2,3,4,5,6};
+            int i;
+            for(i=0;i<5;i++)
+            {
+                if(a[i]%2==0)
+                {
+                    System.out.println("even:"+a[i]);
+                }
+                else
+                {
+                    System.out.println("odd:"+a[i]);
+                }
+            }
+        }
+    }
+                
+            
+        
+    
+    
+
